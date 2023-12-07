e22abc78e751c64455a0902c3d80781f62550c8e
MelekErisgin34 <147922709+MelekErisgin34@users.noreply.github.com>
2023-12-07 16:31:11 +0300

    Add files via upload

---
+++ alımsatım/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alımsatım
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double alısfiyati, satisfiyati, kar;
+            alısfiyati = Convert.ToDouble(textBox1.Text);
+            satisfiyati = Convert.ToDouble(textBox2.Text);
+            kar = satisfiyati-alısfiyati;
+            label3.Text = "Toplam Kar: " + kar;
+        }
+    }
+}
